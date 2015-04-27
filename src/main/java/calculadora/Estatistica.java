f9fd7214db6cf99eb6dcf41d40d92975a485a4a2
nmoreira <nunofrmoreira@gmail.com>
2015-04-27 13:25:49 +0100

    page 1 com historico a funcionar

--- src/main/java/calculadora/Estatistica.java
+++ src/main/java/calculadora/Estatistica.java
@@ -2,15 +2,9 @@ package calculadora;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
-
 import javax.inject.Named;
 
 @Named
